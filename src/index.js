9e2d9161e6f4472de48cfb4bf959d99010b4a016
Ryan DeLosh <ryguy1201@gmail.com>
2017-11-12 14:03:30 -0500

    Production Commit
    Still a small issue with validation but mostly working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 //
 //Name: Ryan DeLosh, ryan_delosh@student.uml.edu
 //Computer Science Department, UMass Lowell Comp.4610, GUI Programming I
-//File: /usr/cs/2018/rdelosh/public_html/midterm Created: 23-Oct-2017
-//Last updated by RD: 23-Oct-2017
+//File: /usr/cs/2018/rdelosh/public_html/hw4 Created: 12-Nov-2017
+//Last updated by RD: 12-Nov-2017
 //Built using ReactJS Library version 16.0.0
 //
 //I decided to use ReactJS for this assignment because I wanted to learn 
@@ -11,6 +11,18 @@
 //a better start out in the job world. Along with becoming a new industry standard
 //React is a very powerful library that make it easy to make some very powerful sites.
 //React is also very friendly with other libraries and frameworks, It is all around useful.
+// ReactJS Library documentation: https://reactjs.org/docs/installation.html
+//Bootstrap documentation: https://getbootstrap.com/docs/4.0/getting-started/introduction/
+//
+//-------HOW TO COMPILE AND RUN-------
+//To run this program it first needs to be built into a file that can be executed on a server or local machine.
+//To start this navigate to the directory containing the src and public folder
+//from there make sure you have NPM installed, directions to install it are here: http://blog.npmjs.org/post/85484771375/how-to-install-npm
+//after npm is installed type in npm install to install the apps dependecies
+//then npm run
+//this will execute a development build on your local system. If you want to publish the app to a web host instead of running npm start type in npm build and that will build a production version of the app.
+
+
 
 //This is the first component to be rendered from here we get passed to our router which will then decide where we should be.
 import React from 'react';
@@ -18,10 +30,7 @@ import ReactDOM from 'react-dom';
 import Routes from './routes';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-//import 'bootstrap/dist/css/bootstrap.css';
-//import 'bootstrap/dist/css/bootstrap-theme.css';
-
-//picks our style sheet and sends us to our router which will decide where we should be
+//Routes and imports the javascript into our html file
 ReactDOM.render((
 	<Router>
 		<Routes />
